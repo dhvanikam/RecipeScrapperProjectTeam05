660264b27f210a9dd23451bfb933122e0a56fb81
alpana2030 <alpana2030@users.noreply.github.com>
2023-04-20 20:14:15 -0500

    added logic to compare

--- src/test/java/utilities/CommonUtilities.java
+++ src/test/java/utilities/CommonUtilities.java
@@ -1,13 +1,27 @@
 package utilities;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
 public class CommonUtilities {
-	//public static WebDriver driver;
-	
+	// public static WebDriver driver;
+
 	public void scrollPage(WebDriver driver) {
-		 JavascriptExecutor js = (JavascriptExecutor) driver;
-		 js.executeScript("window.scrollBy(0,1000)");
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,1000)");
+	}
+
+	public boolean hasEliminateItems(List<String> eleminateItems, String recipeIngredients) {
+
+		for (String avoidItem : eleminateItems) {
+
+			if (recipeIngredients.toUpperCase().contains(avoidItem.toUpperCase())) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
